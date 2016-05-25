5fd2003c7906b020fa53fe23b9e47e1080c55dec
Jose Miguel <jmn8718@gmail.com>
2016-05-26 01:25:41 +0200

    edited template

--- src/api/templates.js
+++ src/api/templates.js
@@ -4,11 +4,11 @@ var path = require('path');
 
 var raml2html = require('raml2html');
 
-router.get('/', function(req, res, next) {
+router.get('/twitterApiDoc', function(req, res, next) {
     var file = 'api.raml';
     var file = 'accounts_v2.raml';
     //var file = 'github.raml';
-    //var file = 'example.raml';
+    // var file = 'example.raml';
 
     var pathfile = path.join(__dirname, '../../resources/raml',file);
 
@@ -22,6 +22,22 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.get('/raml2html', function(req, res, next) {
+    var file = 'api.raml';
+    var file = 'accounts_v2.raml';
+    //var file = 'github.raml';
+    // var file = 'example.raml';
+
+    var pathfile = path.join(__dirname, '../../resources/raml',file);
+
+    var configWithCustomTemplates = raml2html.getDefaultConfig();
+
+    raml2html.render(pathfile, configWithCustomTemplates).then(function(result) {
+        res.end(result)
+    }, function(error) {
+        next(error)
+    });
+});
 
 router.get('/', function(req, res, next) {
     res.render('raml', { title: 'Raml' });
